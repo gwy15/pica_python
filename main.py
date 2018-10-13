74e83bedab884464a2b8cee0f9a2134b0f33d7f8
gwy15 <gwy15thu@gmail.com>
2018-10-13 14:45:32 -0400

    finish main

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 from pica import PicaUser
 from progressbar import progressbar
 
+import argparse
+
 
 def getUser():
     if os.path.exists('token.txt'):
@@ -19,12 +21,22 @@ def getUser():
 if __name__ == "__main__":
     user = getUser()
 
-    # 下载搜索关键词
-    key, page = '', 1
-    for comic in user.search(key, page):
-        user.downloadComic(comic, wrap=progressbar)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', type=str, choices=['search', 'category'])
+    parser.add_argument('keyword', type=str)
+    parser.add_argument('-n', type=int, default=1, help='下载数量')
 
-    # 下载整个分类的一页
-    ctg = ''
-    for comic in user.getCategoryPage(ctg, page=1):
-        user.downloadComic(comic, wrap=progressbar)
+    ns = parser.parse_args()
+
+    if ns.command == 'search':
+        for comic in user.search(ns.keyword, 1)[0:ns.n]:
+            user.downloadComic(comic, wrap=progressbar)
+    elif ns.command == 'category':
+        comics = user.getCategoryPage(ns.keyword, page=1)
+        if not comics:
+            print(f'可选分类: {user.categories()}')
+            exit(1)
+        for comic in comics[0:ns.n]:
+            user.downloadComic(comic, wrap=progressbar)
+    else:
+        raise RuntimeError
